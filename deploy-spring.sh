de12cbc3acbbc10fa8ed6acf51cf145834cf757b
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2024-01-22 21:10:05 +0200

    Update deploy-spring.sh

--- deploy-spring.sh
+++ deploy-spring.sh
@@ -1,1 +1,12 @@
 # TODO, complete deployment of a git project of spring-boot getting input from the command line (port + link)
+
+
+# download maven and add it to path
+
+# download java
+
+# download ufw if not already installed
+
+# configure ufw to allow the spring boot app to expose itself
+
+# run spring boot (TODO find a way to keep a file a certain max size)

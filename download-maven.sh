799f7178f7c77394c8a0490bcaf4c1a07dd02a84
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2024-01-22 20:56:10 +0200

    Update download-maven.sh

--- download-maven.sh
+++ download-maven.sh
@@ -2,7 +2,7 @@ curl -O https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6
 tar -xvzf apache-maven-3.9.6-bin.tar.gz
 sudo mv apache-maven-3.9.6 /opt/
 nano ~/.bashrc
-export M2_HOME=/opt/apache-maven-x.y.z
+export M2_HOME=/opt/apache-maven-3.9.6
 export PATH=$M2_HOME/bin:$PATH
 source ~/.bashrc
 mvn -version

9a6dea87b57354a88bad5ab07a53700419b48a24
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2024-01-23 11:20:47 +0200

    Update install-mkcert.sh

--- install-mkcert.sh
+++ install-mkcert.sh
@@ -1,1 +1,5 @@
-
+sudo apt install libnss3-tools
+curl -s https://api.github.com/repos/FiloSottile/mkcert/releases/latest | grep browser_download_url | grep '\linux-amd64' | cut -d '"' -f 4 | wget -i -
+sudo mv mkcert-v*-linux-amd64 /usr/bin/mkcert
+sudo chmod +x /usr/bin/mkcert
+mkcert --version

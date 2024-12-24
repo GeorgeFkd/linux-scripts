8289ffa81326c0ce898a554a72f6f872fd1ba845
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2024-12-24 14:52:27 +0200

    Update jetbrains-toolbox.sh

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+#OPENSUSE ONLY
 # inspired by https://dev.to/janetmutua/installing-jetbrains-toolbox-on-ubuntu-527f
 VERSION=2.5.2.35332
 echo "Downloading version $VERSION of jetbrains toolbox"
 wget -c https://download.jetbrains.com/toolbox/jetbrains-toolbox-$VERSION.tar.gz
 sudo tar -xzf jetbrains-toolbox-$VERSION.tar.gz -C /opt
+echo "Downloading additional library libgthread-2_0-0"
+sudo zypper install libgthread-2_0-0
 
 echo "run /opt/jetbrains-toolbox-$VERSION/jetbrains-toolbox to run the app"
 
